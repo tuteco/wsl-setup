49d9aa0b3c5d4a0c043eaeed935493ea7907f4e8
Eva Schreck <63257077+evaschreck42@users.noreply.github.com>
2022-10-23 17:21:19 +0200

    Create wsl_install_software.sh

---
+++ wsl_install_software.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+sudo apt-get update \
+&& sudo apt-get install --no-install-recommends -y \
+make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev unzip
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+rm awscliv2.zip
+
+
+rm -rf ~/.pyenv
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+pyenv install 3.10.8 
+pyenv install 3.9.15 
+
+pyenv global 3.10.8
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+rm -rf ~/docker-volumes
+mkdir ~/docker-volumes
+
+rm -rf ~/workspace
+mkdir ~/workspace
