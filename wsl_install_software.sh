babd01bdb8413056e7988e44bc235a2e772f44af
Eva Schreck <63257077+evaschreck42@users.noreply.github.com>
2022-10-23 23:53:11 +0200

    Update wsl_install_software.sh

--- wsl_install_software.sh
+++ wsl_install_software.sh
@@ -7,7 +7,8 @@ sudo apt-get update \
 && sudo apt-get install --no-install-recommends -y \
 make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev unzip
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+unzip rsync
 
 # aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
