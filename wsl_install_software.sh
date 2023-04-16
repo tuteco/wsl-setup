abf1fbbe36957b0457591955818a1ac806b43829
Eva Schreck <eva.schreck@tuteco.de>
2023-04-16 12:51:10 +0200

    new release for testing

--- wsl_install_software.sh
+++ wsl_install_software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # prepare a fresh WSL Ubuntu distro for development of dockerized python applications
-# provied by: .tuteco - the data and knowledge experts
+# provided by: tuteco GmbH - the data and knowledge experts
 
 # update and install required software packages
 sudo apt-get update \
@@ -12,14 +12,15 @@ unzip rsync
 
 # required software tools directories
 mkdir -p .local/bin
+mkdir ~/tmp
 
-# aws cli
+# aws cli installation
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm awscliv2.zip
 
-# saml2aws
+# saml2aws installation
 CURRENT_VERSION=$(curl -Ls https://api.github.com/repos/Versent/saml2aws/releases/latest | grep 'tag_name' | cut -d'v' -f2 | cut -d'"' -f1)
 wget -c https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz -O - | tar -xzv -C ~/.local/bin
 chmod u+x ~/.local/bin/saml2aws
@@ -28,10 +29,11 @@ hash -r
 echo '' >> ~/.profile
 echo 'eval "$(saml2aws --completion-script-bash)"' >> ~/.profile
 
-# pyenv
+# pyenv installation
 rm -rf ~/.pyenv
 curl https://pyenv.run | bash
 
+# add the env variables to .bashrc and .profile
 echo '' >> ~/.bashrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
@@ -42,34 +44,36 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init -)"' >> ~/.profile
 
+# activate the pyenv env variables, so that we can install the python version later
 source ~/.profile
 exec "$SHELL"
 
-# activate the pyenv-virtualenv plugin
-#echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-exec "$SHELL"
-
-# ssh config
+# add ssh config to the profile
 mkdir -p ~/.ssh
+echo '' >> ~/.profile
 curl https://raw.githubusercontent.com/tuteco/wsl-setup/main/profile_ssh_add.txt >> ~/.profile
 
+# add the formatting of the prompt for git branches
+echo '' >> ~/.bashrc
+curl https://raw.githubusercontent.com/tuteco/wsl-setup/main/git_branch_to_brashrc.txt >> ~/.bashrc
+
 # install the desired python versions
-pyenv install 3.10.10
+# we use 3.10 as default version for productive use
+pyenv install 3.11.3
+pyenv install 3.10.11
 pyenv install 3.9.16
-pyenv global 3.10.10
+pyenv global 3.10.11
 
-# pyenv virtualenv py-invoke-basics-3.10
-
-# poetry
+# install poetry
 curl -sSL https://install.python-poetry.org | python3 -
 
 # development directory structure
 mkdir -p ~/docker-volumes
 mkdir -p ~/workspace
 
-# echo "py-invoke-basics-3.10" >> workspace/.python-version
-# cd workspace
-# pip install cookiecutter invoke boto3 docker pytz nbconvert \
-# mkdocs mkdocs-material mkdocs-material-extensions mkdocs-autorefs \
-# mkdocs-gen-files mkdocs-literate-nav mkdocstrings mkdocstrings-python
+# ensure systemd gets started, so that we can use snap to install software
+sudo tee /etc/wsl.conf -a <<_EOF
+[boot]
+systemd=true
+_EOF
+
